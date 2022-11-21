103cc817f6d92633f2a63e958e00ac7af95c976e
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2022-11-21 06:28:32 +0000

    Add append method

--- LinkedList.py
+++ LinkedList.py
@@ -17,8 +17,21 @@ class LinkedList:
             print(temp.value)
             temp = temp.next
 
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.tail = new_node
+            self.head = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
 
 
 if __name__ == '__main__':
     my_linked_list = LinkedList(10)
+    my_linked_list.append(9)
+    my_linked_list.append(8)
+    my_linked_list.append(7)
     my_linked_list.print_lint()
