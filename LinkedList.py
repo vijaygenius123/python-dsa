2774beb30931d3cd0d304e37b75ab184279c83cc
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2022-11-21 14:31:08 +0000

    Add pop and update print

--- LinkedList.py
+++ LinkedList.py
@@ -11,11 +11,15 @@ class LinkedList:
         self.tail = new_node
         self.length = 1
 
-    def print_lint(self):
+    def print(self):
+        list_str = ''
         temp = self.head
-        while temp is not None:
-            print(temp.value)
+        while temp:
+            list_str += str(temp.value)
             temp = temp.next
+            if temp is not None:
+                list_str += ' -> '
+        print(list_str)
 
     def append(self, value):
         new_node = Node(value)
@@ -28,10 +32,32 @@ class LinkedList:
         self.length += 1
         return True
 
+    def pop(self):
+        if self.length == 0:
+            return None
+        else:
+            temp = self.head
+            pre = self.head
+            while temp.next:
+                pre = temp
+                temp = temp.next
+            self.tail = pre
+            self.tail.next = None
+            self.length -= 1
+            if self.length == 0:
+                self.head = None
+                self.tail = None
+            return temp
+
 
 if __name__ == '__main__':
     my_linked_list = LinkedList(10)
     my_linked_list.append(9)
     my_linked_list.append(8)
     my_linked_list.append(7)
-    my_linked_list.print_lint()
+    my_linked_list.print()
+    my_linked_list.pop()
+    my_linked_list.pop()
+    my_linked_list.print()
+    my_linked_list.pop()
+    my_linked_list.print()
